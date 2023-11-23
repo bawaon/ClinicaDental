13802c1c53811a3eb14d1c35a7105eab0d691ace
MexicoLayonzz <147360211+MexicoLayonzz@users.noreply.github.com>
2023-11-23 10:28:10 -0600

    Add files via upload

---
+++ Genero.java
@@ -0,0 +1,6 @@
+package proyecto.backend;
+
+public enum Genero 
+{
+	MUJER, HOMBRE, NOBINARIO, OTRO
+}
---
+++ NumConsultorio.java
@@ -0,0 +1,6 @@
+package proyecto.backend;
+
+public enum NumConsultorio 
+{
+	UNO, DOS, TRES, CUATRO
+}
